24681be08a814da1dcd9215babf03aefd37cab7c
dougtebay <dougtebay@gmail.com>
2015-12-13 21:54:07 -0500

    Add validations for descriptions

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ActiveRecord::Base
 validates :title, presence: true
 validates :title, length: { minimum: 3 }
+validates :desciption, presence: true
+validates :desciption, length: { minimum: 5 }
 end
